2dd3225bd4c3a332f60edbd138ed9bbcf9beef33
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 12:24:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -63,9 +63,6 @@ end
 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
   i = 0
   while i < cart.length do 
     if cart[i][:clearance] == true 
